b7e9df3e94018a1e5c621447c850169f8faa5105
borodedamie <44114790+borodedamie@users.noreply.github.com>
2023-01-16 05:21:51 -0600

    set up mongoose and connect to my mongo DB atlas

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`drugstore app listening on port ${port}`)
 })
