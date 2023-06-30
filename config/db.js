53ee6fccb271cdcbdbfee17d4b2fb96ea79394bf
Onaopemipo Oluborode <44114790+borodedamie@users.noreply.github.com>
2023-06-30 10:45:06 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500, 
+  connectTimeoutMS: 10000,
+};
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL)
+        const conn = await mongoose.connect(process.env.MONGO_URL, options)
 
         console.log(`DB connected ${ conn.connection.host}`)
     } catch (error) {
